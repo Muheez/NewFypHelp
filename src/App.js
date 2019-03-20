a9dc320a8e9d0ab5ebfa0cc50c56b4acba19362c
muheez <muheezakanni@gmail.com>
2019-03-20 02:36:21 +0100

    add copyright and increase search limit

--- src/App.js
+++ src/App.js
@@ -14,36 +14,39 @@ class App extends Component {
     e.persist();
     let value = e.target.value;
 
-    console.log(value);
-
     if(value === "")
-      return;
+      return this.setState({ results: [] });
     //fetch data with user query input
-    fetch(`https://clinicaltables.nlm.nih.gov/api/conditions/v3/search?terms=${value}&df=primary_name,info_link_data`, {
+    fetch(`https://clinicaltables.nlm.nih.gov/api/conditions/v3/search?terms=${value}&df=primary_name,info_link_data&maxList=20`, {
       method: "GET",
     })
     .then(response => {
       if(response.status === 200 || response.status === 201) {
         response.json()
         .then(res => {
-          console.log(res);
+          this.setState({ results: res[3] })
         });
       }
     })
   }
   
   render() {
+    let results = this.state.results.map(result => {
+      return <SearchResult text={result[0]} link={result[1].substring(0, result[1].indexOf(","))} />
+    })
+
     return (
       <div>
         <div class="main-container">
           <h3 class="app-header">FYP<span>help.</span></h3>
-          <div class="search-engine active">
+          <div class={this.state.results.length === 0 ? "search-engine" : "search-engine active"}>
             <div class="search-icon"></div>
             <input onInput={this.handleQueryInput.bind(this)} type="text" placeholder="Search disease" class="input-field" />
             <ul class="search-results">
-              <SearchResult text="Gastroesophageal reflux disease" link="#" />
+              {results}
             </ul>
           </div>
+          <p class="copyright">copyright 2019 | all rights reserved | FYPhelp.</p>
         </div>
       </div>
     );
@@ -53,7 +56,7 @@ class App extends Component {
 class SearchResult extends Component {
   render() {
     return(
-      <li class="result"><a href={this.props.link}>{this.props.text}</a></li>
+      <a href={this.props.link} rel="noopener noreferrer" target="_blank"><li class="result">{this.props.text}</li></a>
     )
   }
 }
