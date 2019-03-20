75dfcd4c2425e662531f739bd9ca0f1a4bbe3cb2
muheez <muheezakanni@gmail.com>
2019-03-20 02:02:52 +0100

    initial algorithm state

--- src/App.js
+++ src/App.js
@@ -2,22 +2,59 @@ import React, { Component } from 'react';
 import "./style/styles.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      results: []
+    }
+  }
+
+  handleQueryInput(e) {
+    e.persist();
+    let value = e.target.value;
+
+    console.log(value);
+
+    if(value === "")
+      return;
+    //fetch data with user query input
+    fetch(`https://clinicaltables.nlm.nih.gov/api/conditions/v3/search?terms=${value}&df=primary_name,info_link_data`, {
+      method: "GET",
+    })
+    .then(response => {
+      if(response.status === 200 || response.status === 201) {
+        response.json()
+        .then(res => {
+          console.log(res);
+        });
+      }
+    })
+  }
+  
   render() {
     return (
       <div>
         <div class="main-container">
           <h3 class="app-header">FYP<span>help.</span></h3>
-          <div class="search-engine">
+          <div class="search-engine active">
             <div class="search-icon"></div>
-            <input type="text" placeholder="Search disease" class="input-field" />
+            <input onInput={this.handleQueryInput.bind(this)} type="text" placeholder="Search disease" class="input-field" />
             <ul class="search-results">
-              <li class="result"><a href="#">Gastroesophageal reflux disease</a></li>
-              <li class="result"><a href="#">Gastroenteritis</a></li>
+              <SearchResult text="Gastroesophageal reflux disease" link="#" />
             </ul>
           </div>
         </div>
       </div>
     );
+  }
+}
+
+class SearchResult extends Component {
+  render() {
+    return(
+      <li class="result"><a href={this.props.link}>{this.props.text}</a></li>
+    )
   }
 }
 
